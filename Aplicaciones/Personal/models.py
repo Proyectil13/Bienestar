19006313b9bace972662d130ab114bbb260b4117
unknown <luismarino123>
2024-08-22 02:35:15 -0400

    Class personal

--- Aplicaciones/Personal/models.py
+++ Aplicaciones/Personal/models.py
@@ -5,4 +5,17 @@ from django.db import models
 
 class Personal (models.Model):
     
-    cedula = models.IntegerField()
+    Cedula = models.IntegerField(max_length=8, primary_key=True)
+    Apellidos= models.CharField(max_length=40)
+    Nombres = models.CharField(max_length=40)
+    EdoCiviles= [
+        ('S', 'Soltero(a)'),
+        ('C', 'Casado(a)'),
+        ('D', 'Divorcidado(a)'),
+        ('V', 'Viudo(a)' ),
+        ('O', 'Otro')
+
+    ]
+    EdoCivil = models.CharField(max_length=1, choices=EdoCiviles, default= "S")
+
+
